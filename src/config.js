fde676f5862c58b28adc61e08f1cefb26a08a2ba
Astitva Gupta <gupta.astitva@gmail.com>
2021-04-14 00:30:02 +0530

    1. Changed video codec to VP9 for better streaming 2. added 'profile-id' in video field for 10 bit color 3. added 'useinbandfec' to minimise lossy packets

--- src/config.js
+++ src/config.js
@@ -1,64 +1,66 @@
 const os = require('os')
 
 module.exports = {
-    listenIp: '0.0.0.0',
-    listenPort: 3016,
-    sslCrt: '../ssl/cert.pem',
-    sslKey: '../ssl/cert.key',
-    
-    mediasoup: {
-      // Worker settings
-      numWorkers : Object.keys(os.cpus()).length,
-      worker: {
-        rtcMinPort: 10000,
-        rtcMaxPort: 10100,
-        logLevel: 'warn',
-        logTags: [
-          'info',
-          'ice',
-          'dtls',
-          'rtp',
-          'srtp',
-          'rtcp',
-          // 'rtx',
-          // 'bwe',
-          // 'score',
-          // 'simulcast',
-          // 'svc'
-        ],
-      },
-      // Router settings
-      router: {
-        mediaCodecs:
-          [
+  listenIp: '0.0.0.0',
+  listenPort: 3016,
+  sslCrt: '../ssl/cert.pem',
+  sslKey: '../ssl/cert.key',
+
+  mediasoup: {
+    // Worker settings
+    numWorkers: Object.keys(os.cpus()).length,
+    worker: {
+      rtcMinPort: 10000,
+      rtcMaxPort: 10100,
+      logLevel: 'warn',
+      logTags: [
+        'info',
+        'ice',
+        'dtls',
+        'rtp',
+        'srtp',
+        'rtcp',
+        // 'rtx',
+        // 'bwe',
+        // 'score',
+        // 'simulcast',
+        // 'svc'
+      ],
+    },
+    // Router settings
+    router: {
+      mediaCodecs:
+        [
+          {
+            kind: 'audio',
+            mimeType: 'audio/opus',
+            clockRate: 48000,
+            channels: 2,
+            useinbandfec: 1
+          },
+          {
+            kind: 'video',
+            mimeType: 'video/VP9',
+            clockRate: 90000,
+            'profile-id': 2,
+            parameters:
             {
-              kind: 'audio',
-              mimeType: 'audio/opus',
-              clockRate: 48000,
-              channels: 2
-            },
-            {
-              kind: 'video',
-              mimeType: 'video/VP8',
-              clockRate: 90000,
-              parameters:
-                {
-                  'x-google-start-bitrate': 1000
-                }
-            },
-          ]
-      },
+              'x-google-start-bitrate': 1000
+            }
+          },
+        ]
+    },
     // WebRtcTransport settings
     webRtcTransport: {
-        listenIps: [
-          {
-            ip: '0.0.0.0',      
-            announcedIp:'127.0.0.1' // replace by public IP address
-          }
-        ],
-        maxIncomingBitrate: 1500000,
-        initialAvailableOutgoingBitrate: 1000000
+      listenIps: [
+        {
+          ip: '0.0.0.0',
+          announcedIp: '10.1.102.27' // replace by public IP address
+        }
+      ],
+      maxIncomingBitrate: 15000000,
+      initialAvailableOutgoingBitrate: 1000000
     },
-    }
-  };
-  
+  }
+};
+
